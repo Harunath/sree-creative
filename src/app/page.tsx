74c25b810443c9b44d57d8562a0368a35337b8bb
sushmasri1906 <sushmasri1906@gmail.com>
2025-06-14 16:07:16 +0530

    icons updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/home/Hero";
+import IconBar from "@/components/home/IconBar";
 import OurServices from "@/components/home/OurServices";
 
 export default function Home() {
 	return (
 		<div className=" min-h-screen">
 			<Hero />
+			<IconBar />
 			<OurServices />
 		</div>
 	);
